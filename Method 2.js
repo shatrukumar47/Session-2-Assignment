114063c6fe8b828ca3edae875c39da33bdbbd41a
shatrukumar47 <shatrukumar47@gmail.com>
2023-02-07 15:17:54 +0530

    I tried new method

---
+++ Method 2.js
@@ -0,0 +1,12 @@
+let number = 11;
+let count = 0;
+for(let i=1; i<=number; i++){
+    if(number%i===0){
+        count++;
+    }
+}
+if(count===2){
+    console.log("Prime Number");
+}else{
+    console.log("Not Prime");
+}
